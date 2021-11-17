921cfd12ea8b75804528e88d9edd6b48c114417e
csc530 <cscousin@lakeheadu.ca>
2021-11-17 09:49:32 -0500

    created initial start scene

---
+++ src/main/java/space/nasa/spaceapi/controllers/Search.java
@@ -0,0 +1,12 @@
+package space.nasa.spaceapi.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Search implements Initializable{
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle){
+	}
+}
