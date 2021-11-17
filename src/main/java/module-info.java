1a91afaab34b1e7dd8043b3773610a8bd1d45e61
csc530 <cscousin@lakeheadu.ca>
2021-11-17 09:26:42 -0500

    initiail commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module space.nasa.spaceapi {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.web;
+	requires org.controlsfx.controls;
+	requires com.dlsc.formsfx;
+	requires validatorfx;
+	requires org.kordamp.ikonli.javafx;
+	requires org.kordamp.bootstrapfx.core;
+	requires eu.hansolo.tilesfx;
+	opens space.nasa.spaceapi to javafx.fxml;
+	exports space.nasa.spaceapi;
+}
---
+++ src/main/java/space/nasa/spaceapi/utilities/Transition.java
@@ -0,0 +1,21 @@
+package space.nasa.spaceapi.utilities;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import space.nasa.spaceapi.Main;
+
+import java.io.IOException;
+
+public class Transition{
+	public static void to(ActionEvent event, String fxmlFile, String title) throws IOException{
+		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/" + fxmlFile));
+		Scene scene = new Scene(fxmlLoader.load());
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setTitle(title);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
