64503799166b949d879216d39466e38cf9d671ac
csc530 <cscousin@lakeheadu.ca>
2021-11-21 19:32:00 -0500

    Cleaned up search view and added functionality to choose a random amount of APODs

--- src/main/java/space/nasa/spaceapi/controllers/searchController.java
+++ src/main/java/space/nasa/spaceapi/controllers/searchController.java
@@ -30,21 +30,44 @@ public class searchController implements Initializable{
 	@FXML
 	private ListView<APOD> apods;
 	
+	/**
+	 * Returns a random valid LocalDate which an astronmy picture of the day was posted
+	 * @param daysFromPresent   the upper bound of date, the amount to subtract to allow for the most recent possible
+	 *                          date to be returned. How many days ago should the upper bound be?
+	 * @param daysFromBeginning the lower bound of date. How many days after the first posted astronomy picture of
+	 *                          day should the lower bound be?
+	 * @return a valid random LocalDate between given param bounds for NASA's astronomy picture of the day. Returns
+	 * 		null if no possible could be returned with the given params
+	 */
+	public static LocalDate getRandomDateInAPOD(int daysFromPresent, int daysFromBeginning){
+		long today = LocalDate.now().toEpochDay();
+		long startDate = minDate.toEpochDay();
+		return LocalDate.ofEpochDay((long) (Math.random() * (today - startDate - daysFromPresent) + (daysFromBeginning + startDate)));
+	}
+	
 	@FXML
 	void back(ActionEvent event){
 	}
 	
 	@FXML
 	void rSearch(ActionEvent event){
+		apods.getItems().clear();
 		apods.getItems().addAll((API.getAPODs(start.getValue(), end.getValue())));
 	}
 	
 	@FXML
 	void random(ActionEvent event){
-//		apods.getItems().clear();
-//		if(count.getValue() > 1)
-//			apods.getItems().addAll(API.getAPODs());
-//		else apods.getItems().addAll(API.getAPOD());
+		LocalDate rDate = getRandomDateInAPOD(0, 0);
+		apods.getItems().clear();
+		final Integer countValue = count.getValue();
+		if(countValue < 2)
+			apods.getItems().add(API.getAPOD(rDate));
+		else
+		{
+			rDate = getRandomDateInAPOD(countValue, 0);
+			//minus 1 to account that start date count as one as well as end date
+			apods.getItems().addAll(API.getAPODs(rDate, rDate.plusDays(countValue-1)));
+		}
 	}
 	
 	@FXML
@@ -55,13 +78,15 @@ public class searchController implements Initializable{
 	
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle){
+		int amount = (int) (Math.random()*7);
+		LocalDate rDate = getRandomDateInAPOD(0, amount);
 		count.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 1000, 1));
 		addDateChecker(start);
 		addDateChecker(date);
 		addDateChecker(end);
-		start.setValue(LocalDate.now().minusDays(7));
-		end.setValue(LocalDate.now());
-		date.setValue(LocalDate.ofEpochDay((long) (Math.random() *(LocalDate.now().toEpochDay()-minDate.toEpochDay()  ) + minDate.toEpochDay())));
+		start.setValue(rDate.minusDays(amount));
+		end.setValue(rDate);
+		date.setValue(getRandomDateInAPOD(0, 0));
 	}
 	
 	public void addDateChecker(DatePicker datepicker){
--- src/main/java/space/nasa/spaceapi/models/APOD.java
+++ src/main/java/space/nasa/spaceapi/models/APOD.java
@@ -8,9 +8,11 @@ import java.text.DateFormat;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Locale;
+import java.util.StringJoiner;
 
 //astronomy picture of the day
 public class APOD{
+	public static final String[] mediaTypes = {"video", "image"};
 	private Date date;
 	private String explanation;
 	@SerializedName("hdurl")
@@ -21,18 +23,22 @@ public class APOD{
 	private String copyright;
 	@SerializedName("thumbnail_url")
 	private URL thumbnail;
+	private String title;
+	private URL url;
 	
-	public static final String[] mediaTypes= {"video","image"} ;
+	public APOD(){}
+	
 	public URL getThumbnail(){
 		return thumbnail;
 	}
 	
-	public APOD(){}
-	
 	public LocalDate getDate(){
+		//i need to convert to lacaldate instead of maikng the datatype local date because java.time module "doEsn'T
+		// oPeN tO th gson
+		
 		//Converts date which opens to gson to LocalDate (which doesn't) so subtract and add and better functions can be performed
 		// divide 60 60 24 to convert the epochSecond to epoch Day for the method to work correctly
-		return LocalDate.ofEpochDay(date.toInstant().getEpochSecond()/60/60/24);
+		return LocalDate.ofEpochDay(date.toInstant().getEpochSecond() / 60 / 60 / 24);
 	}
 	
 	public String getExplanation(){
@@ -59,24 +65,28 @@ public class APOD{
 		return url;
 	}
 	
-	private String title;
-	private URL url;
 	public Image getImage(){
 		if(mediaType.equals(mediaTypes[0]))
 			return new Image(String.valueOf(thumbnail));
 		URL imgUrl = (hdUrl == null) ? url : hdUrl;
 		return new Image(imgUrl.toString());
 	}
+	
 	public URL getVideo(){
 		return mediaType.equals(mediaTypes[1]) ? null : url;
 	}
+	
 	public String getDateString(){
-		return		                                DateFormat.getDateInstance(DateFormat.FULL, Locale.getDefault()).format(date);
+		return DateFormat.getDateInstance(DateFormat.FULL, Locale.getDefault()).format(date);
 	}
+	
 	@Override
 	public String toString(){
-		return "%s\n%s\n%s\n".formatted(title,
-		                                explanation,
-		                                getDateString());
+		StringJoiner joiner = new StringJoiner(" ", "", "...");
+		String[] s = explanation.split(" ");
+		//if they add 5 elements or reach the end of the description string
+		for(int i = 0; (i < 5) && (i < s.length); i++)
+			joiner.add(s[i]);
+		return "%s\n%s\n%s\n".formatted(getDateString(), title, joiner.toString());
 	}
 }
